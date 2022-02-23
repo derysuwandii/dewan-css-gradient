789a5176fb144859b61ebefb6f61890d598c5785
Dewan Komputer <dewan.komputer@gmail.com>
2022-02-24 00:10:35 +0700

    first commit

---
+++ gradien.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300" type="text/css" />
+<style type="text/css">
+	#gradient1 {
+	  background-image: linear-gradient(yellow, red);
+	}
+</style>
+<style type="text/css">
+	#gradient2 {
+	  background-image: linear-gradient(to right, #4682B4, #00FFFF, #00FA9A);
+	}
+</style>
+<style type="text/css">
+	#gradient3 {
+	  background-image: linear-gradient(to bottom right, #FF0000, #FFC0CB);
+	}
+</style>
+<style type="text/css">
+	#gradient4 {
+	  background-image: linear-gradient(-90deg, red, yellow);
+	}
+</style>
+<style type="text/css">
+	#gradient5 {
+	  background-image: linear-gradient(red, #FF1493, rgba(255,192,203));
+	}
+</style>
+<style type="text/css">
+	#gradient6 {
+	  background-image: linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet);
+	}
+</style>
+<style type="text/css">
+	#gradient7 {
+	  background-image: linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1));
+	}
+</style>
+<style type="text/css">
+	#gradient8 {
+	  background-image: repeating-linear-gradient(#00FA9A, #FFFF00 10%, #FF4500 20%);
+	}
+</style>
+<style type="text/css">
+	#gradient9 {
+	  background-image: radial-gradient(red, orange, yellow);
+	}
+</style>
+<style type="text/css">
+	#gradient10 {
+	  background-image: radial-gradient(red 5%, yellow 15%, green 60%);
+	}
+</style>
+<style type="text/css">
+	#gradient11 {
+	  background-image: radial-gradient(circle, red, yellow, green);
+	}
+</style>
+<style type="text/css">
+	#gradient12 {
+	  background-image: radial-gradient(closest-side at 60% 55%, red, yellow, black);
+	}
+</style>
+<style type="text/css">
+	#gradient13 {
+	  background-image: radial-gradient(farthest-side at 60% 55%, red, yellow, black);
+	}
+</style>
+<style type="text/css">
+	#gradient14 {
+	  background-image: repeating-radial-gradient(red, yellow 10%, green 15%);
+	}
+</style>
+<style type="text/css">
+	div {
+		height: 300px;
+		width: 100%;
+		padding-bottom: 20px;
+	}
+</style>
+</head>
+<body>
+  <h4>Gradien 1</h4>
+  <div id="gradient1"></div>
+
+  <h4>Gradien 2</h4>
+  <div id="gradient2"></div>
+
+  <h4>Gradien 3</h4>
+  <div id="gradient3"></div>
+
+  <h4>Gradien 4</h4>
+  <div id="gradient4"></div>
+
+  <h4>Gradien 5</h4>
+  <div id="gradient5"></div>
+
+  <h4>Gradien 6</h4>
+  <div id="gradient6"></div>
+
+  <h4>Gradien 7</h4>
+  <div id="gradient7"></div>
+
+  <h4>Gradien 8</h4>
+  <div id="gradient8"></div>
+
+  <h4>Gradien 9</h4>
+  <div id="gradient9"></div>
+
+  <h4>Gradien 10</h4>
+  <div id="gradient10"></div>
+
+  <h4>Gradien 11</h4>
+  <div id="gradient11"></div>
+
+  <h4>Gradien 12</h4>
+  <div id="gradient12"></div>
+
+  <h4>Gradien 13</h4>
+  <div id="gradient13"></div>
+
+  <h4>Gradien 14</h4>
+  <div id="gradient14"></div>
+</body>
+</html>
